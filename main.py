357b1834d893b75a960b7142dbab22e19cc249ad
AlexStavrev <40688355+AlexStavrev@users.noreply.github.com>
2025-06-04 22:25:19 +0200

    feat: initial grovers
    Co-Authored-By: Mihail Gerginov <mihailgerginov01@gmail.com>

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+from qiskit import QuantumCircuit, QuantumRegister
+from dotenv import load_dotenv
+import os
+
+from qiskit_aer import AerSimulator
+from qiskit_ibm_runtime import QiskitRuntimeService
+
 def main():
     print("Hello from qic-project!")
 
